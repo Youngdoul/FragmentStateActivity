d67785b3d3adeff182fed52cce9eb9a72f54f1a0
Youngdoul <157045613+Youngdoul@users.noreply.github.com>
2024-03-19 12:04:43 -0400

    Fixed the save instance and the view pro

--- app/src/main/java/edu/temple/diceroll/DiceFragment.kt
+++ app/src/main/java/edu/temple/diceroll/DiceFragment.kt
@@ -10,7 +10,7 @@ import android.widget.TextView
 import kotlin.random.Random
 
 const val DIE_SIDES = "dIcE_SiDeS"
-
+private var lastRolled: Int? = null
 class DiceFragment : Fragment() {
     private var sides: Int? = null
 
@@ -19,7 +19,18 @@ class DiceFragment : Fragment() {
         arguments?.let {
             sides = it.getInt(DIE_SIDES)
         }
+        if (savedInstanceState != null) {
+            lastRolled = savedInstanceState.getInt("LAST_ROLLED_NUMBER", 0)
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        lastRolled?.let {
+            outState.putInt("LAST_ROLLED_NUMBER", it)
+        }
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,9 +39,15 @@ class DiceFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_dice, container, false).apply {
 
             val numberDisplayTextView = findViewById<TextView>(R.id.numberDisplay)
+
+            numberDisplayTextView.text = lastRolled?.toString() ?: "0"
             findViewById<Button>(R.id.rollButton).setOnClickListener {
-                numberDisplayTextView.text = (Random.nextInt(sides!!) + 1).toString()
+
+
+                lastRolled = (Random.nextInt(sides ?: 6) + 1)
+                numberDisplayTextView.text = lastRolled.toString()
             }
+            return view
         }
     }
 
